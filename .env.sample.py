bb02caf73341eea87242d82f80b996885adef895
somyajeet1 <92727905+somyajeet1@users.noreply.github.com>
2021-11-01 12:31:28 +0530

    Add files via upload

---
+++ .env.sample.py
@@ -0,0 +1,11 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+API_ID=12345
+API_HASH=abc123
+BOT_TOKEN=123:abc
+SUDO_USERS=123 356 789
+REPLY_MESSAGE=No PMs
+SESSION_STRING=session
+SUPPORT_GROUP=teamDlt
+UPDATES_CHANNEL=teamDlt_update
+ASSISTANT_NAME=pokimanevideo
