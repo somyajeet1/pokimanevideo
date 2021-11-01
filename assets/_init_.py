a84bba0dcf69fddb28c2ab3fd45acfca3d5b792f
somyajeet1 <92727905+somyajeet1@users.noreply.github.com>
2021-11-01 12:25:39 +0530

    Add files via upload

---
+++ assets/_init_.py
@@ -0,0 +1,20 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"""
+pokimanevideo, Telegram Video Chat Bot
+Copyright (c) 2021  Asm Safone <https://github.com/somyajeet1>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from assets.admins import admins, get, set
+
+__all__ = ["admins", "get", "set"]
